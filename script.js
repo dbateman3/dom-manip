0227bb94c366ddc5bbe1a540bfbe9870b1c9e964
Dan Bateman <dbateman0319@starkstate.net>
2017-11-24 22:33:54 -0500

    Added content

--- script.js
+++ script.js
@@ -10,14 +10,15 @@ tab3.addEventListener("click", function(){setContent("tab3", "content3")});
 
 
 function setContent(tabName, contentName) {
+	//remove active class from all tabs
 	for (i = 0; i < tabs.length; i++) {
 		tabs[i].className = tabs[i].className.replace("active", "");
 	}
-
+	//remove active class from all content
 	for (i = 0; i < content.length; i++) {
 		content[i].className = content[i].className.replace("active", "");
 	}
-	
+	//find tab and content and add class active
 	document.getElementById(tabName).setAttribute("class", "tab active");
 	document.getElementById(contentName).setAttribute("class", "content active");
 	
