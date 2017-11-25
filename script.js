554b0386ccdb9dea9cfb9447d2a40b78e1fb60cf
Dan Bateman <dbateman0319@starkstate.net>
2017-11-24 22:07:12 -0500

    Javascript working correctly

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ let tab3 = document.getElementById('tab3');
 let tabs = document.getElementsByClassName("tab");
 let content = document.getElementsByClassName("content");
 
-tab1.addEventListener("click", setContent("tab1", "content1"));
-tab2.addEventListener("click", setContent("tab2", "content2"));
-tab3.addEventListener("click", setContent("tab3", "content3"));
+tab1.addEventListener("click", function(){setContent("tab1", "content1")});
+tab2.addEventListener("click", function(){setContent("tab2", "content2")});
+tab3.addEventListener("click", function(){setContent("tab3", "content3")});
 
 
 function setContent(tabName, contentName) {
 	for (i = 0; i < tabs.length; i++) {
-		tabs[i].className = tabs[i].className.replace(" active", "");
+		tabs[i].className = tabs[i].className.replace("active", "");
 	}
 
 	for (i = 0; i < content.length; i++) {
-		content[i].className = content[i].className.replace(" active", "");
+		content[i].className = content[i].className.replace("active", "");
 	}
-	document.getElementById(tabName).setAttribute("class", " active");
-	document.getElementById(contentName).setAttribute("class", " active");
+	
+	document.getElementById(tabName).setAttribute("class", "tab active");
+	document.getElementById(contentName).setAttribute("class", "content active");
+	
 }
